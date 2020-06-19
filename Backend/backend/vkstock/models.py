eded861e3d1d32822fb47fd5997917989cba6f9f
Dmitry Skupov <skupov2d@mail.ru>
2020-06-19 19:54:51 +0300

    Init backend models

--- Backend/backend/vkstock/models.py
+++ Backend/backend/vkstock/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    """Type of platform for getting and posting posts.
+    """
+    name = models.CharField(max_length=150)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    type_id = models.ForeignKey(Type, on_delete=models.CASCADE)
+
+
+class Worker(models.Model):
+    token = models.CharField(max_length=1000)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    type_id = models.ForeignKey(Type, on_delete=models.CASCADE)
+
+
+class Source(models.Model):
+    name = models.CharField(max_length=200)
+    platform_id = models.CharField(max_length=1000)
+    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    last_record_id = models.CharField(max_length=1000)
+
+
+class Post(models.Model):
+    platform_id = models.CharField(max_length=1000)
+
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=200)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
