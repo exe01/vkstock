b02e1d66a0b7b2cd0c37031d33d039f314204b25
Dmitry Skupov <skupov2d@mail.ru>
2020-06-19 21:56:13 +0300

    Refactored models

--- Backend/backend/vkstock/models.py
+++ Backend/backend/vkstock/models.py
@@ -1,34 +1,51 @@
 from django.db import models
 
 
+# CHANGE TYPE OF DELETE RENDERED POST
+
 class Type(models.Model):
     """Type of platform for getting and posting posts.
     """
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=32)
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=200)
-    type_id = models.ForeignKey(Type, on_delete=models.CASCADE)
-
-
-class Worker(models.Model):
-    token = models.CharField(max_length=1000)
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
     type_id = models.ForeignKey(Type, on_delete=models.CASCADE)
 
 
 class Source(models.Model):
-    name = models.CharField(max_length=200)
-    platform_id = models.CharField(max_length=1000)
-    worker_id = models.ForeignKey(Worker, on_delete=models.CASCADE)
-    last_record_id = models.CharField(max_length=1000)
+    name = models.CharField(max_length=256)
+    platform_id = models.CharField(max_length=512)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    last_record_id = models.CharField(max_length=512)
+    type_id = models.ForeignKey(Type, on_delete=models.CASCADE)
 
 
 class Post(models.Model):
-    platform_id = models.CharField(max_length=1000)
+    platform_id = models.CharField(max_length=512)
+    source_id = models.ForeignKey(Source, on_delete=models.CASCADE)
+    text = models.TextField()
+
+
+class PostImage(models.Model):
+    path = models.CharField(max_length=512)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    user_name = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=256)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class RenderedPost(models.Model):
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    platform_id = models.CharField(max_length=512)
+    text = models.TextField()
+
+
+class RenderedImage(models.Model):
+    rendered_post_id = models.ForeignKey(RenderedPost, on_delete=models.CASCADE)
+    path = models.CharField(max_length=512)
+
