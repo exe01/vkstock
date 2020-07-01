020c7898106dfb39022c527ce5d65b4d510f5feb
Dmitry Skupov <skupov2d@mail.ru>
2020-07-01 23:54:53 +0300

    Refactored image builded added ordering for posts

--- backend/backend/stock_api/image_builder.py
+++ backend/backend/stock_api/image_builder.py
@@ -2,19 +2,32 @@ from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
 import random
 import string
+import textwrap
 
 
 class ImageBuilder:
     def build(self, original_img, text):
+        lines = textwrap.wrap(text, width=40)
+        text = '\n'.join(lines)
+
+        font = ImageFont.truetype("fonts/tahoma.ttf", 40)
+        test_img = Image.new('RGB', (10, 10), color="white")
+        test_img_driwer = ImageDraw.Draw(test_img)
+        max_width, max_height = test_img_driwer.multiline_textsize(text, font=font)
+
         xsize, ysize = original_img.size
-        offset = 100
+
+        if max_width > xsize:
+            xsize = max_width
+
+        offset = max_height + 40
 
         img_with_white_box = Image.new('RGB', (xsize, ysize+offset), color="white")
         img_with_white_box.paste(original_img, (0, offset))
 
         img_driwer = ImageDraw.Draw(img_with_white_box)
-        font = ImageFont.truetype("fonts/tahoma.ttf", 40)
-        img_driwer.text((10, 10), text, fill=(0, 0, 0), font=font)
+
+        img_driwer.multiline_text((10, 10), text, fill=(0, 0, 0), font=font, align='left')
 
         return img_with_white_box
 
